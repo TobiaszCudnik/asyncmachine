d9ffb7a026ec80bf8de200b05a74fbb7ad733a78
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2014-06-15 21:15:27 +0200

    cleanupsˆˆ

--- build/asyncmachine.d.ts
+++ build/asyncmachine.d.ts
@@ -1,11 +1,12 @@
 /// <reference path="../d.ts/es5-shim.d.ts" />
 /// <reference path="../d.ts/rsvp.d.ts" />
 /// <reference path="../d.ts/lucidjs.d.ts" />
-/// <reference path="../d.ts/commonjs.d.ts" />
-import lucidjs = require("lucidjs");
-import rsvp = require("rsvp");
-export declare var Promise: typeof rsvp.Promise;
-export declare class AsyncMachine extends lucidjs.EventEmitter {
+
+declare module "asyncmachine" {
+	export import lucidjs = require("lucidjs");
+	export import rsvp = require("rsvp");
+	export var Promise: typeof rsvp.Promise;
+	export class AsyncMachine extends lucidjs.EventEmitter {
     public config: any;
     private states_all;
     private states_active;
@@ -14,10 +15,7 @@ export declare class AsyncMachine extends lucidjs.EventEmitter {
     public last_promise: rsvp.Promise;
     public log_handler_: any;
     public debug_prefix: string;
-<<<<<<< HEAD
     public debug_level: number;
-=======
->>>>>>> Updated deps and makefile.
     private clock_;
     private debug_;
     constructor(config?: any);
@@ -49,58 +47,42 @@ export declare class AsyncMachine extends lucidjs.EventEmitter {
     public pipeInvert(state: string, machine: AsyncMachine, target_state: string): lucidjs.IBinding;
     public pipeOff(): void;
     public namespaceName(state: string): string;
-<<<<<<< HEAD
     public debug(prefix?: any, level?: any, handler?: any): any;
     public log(msg: string, level?: number): void;
     private processAutoStates(excluded?);
     private setState_(states, params);
     private addState_(states, params);
     private dropState_(states, params);
-=======
-    public debug(prefix?: any, handler?: any): any;
-    public log(...msgs: any[]): any;
-    private processAutoStates(excluded?);
-    private setState_(states, exec_params, callback_params?);
-    private addState_(states, exec_params, callback_params?);
-    private dropState_(states, exec_params, callback_params?);
->>>>>>> Updated deps and makefile.
     private processQueue_(previous_ret);
     private allStatesSet(states);
     private allStatesNotSet(states);
     private createCallback(deferred);
     private namespaceTransition_(transition);
-<<<<<<< HEAD
     private selfTransitionExec_(states, params?);
-=======
-    private selfTransitionExec_(states, exec_params?, callback_params?);
->>>>>>> Updated deps and makefile.
     private setupTargetStates_(states, exclude?);
     private parseImplies_(states);
     private parseRequires_(states);
     private removeDuplicateStates_(states);
     private isStateBlocked_(states, name);
-<<<<<<< HEAD
     private transition_(to, explicit_states, params?);
-=======
-    private transition_(to, explicit_states, exec_params?, callback_params?);
->>>>>>> Updated deps and makefile.
     private setActiveStates_(target);
     private transitionExit_(from, to, explicit_states, params);
     private transitionEnter_(to, target_states, params);
     private transitionExec_(method, target_states, params?);
     private orderStates_(states);
-}
-export interface IState {
-    depends?: string[];
-    implies?: string[];
-    blocks?: string[];
-    requires?: string[];
-    auto?: boolean;
-}
-export interface IConfig {
-    debug: boolean;
-}
-export interface ITransition {
-    call(states?: string[], state_params?: any[], callback_params?: any[]): boolean;
-    apply(context: any, args: any): any;
+	}
+	export interface IState {
+	    depends?: string[];
+	    implies?: string[];
+	    blocks?: string[];
+	    requires?: string[];
+	    auto?: boolean;
+	}
+	export interface IConfig {
+	    debug: boolean;
+	}
+	export interface ITransition {
+	    call(states?: string[], state_params?: any[], callback_params?: any[]): boolean;
+	    apply(context: any, args: any): any;
+	}
 }
