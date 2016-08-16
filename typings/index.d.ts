eb19f7742e97eca1541d44de46a01318f65f4650
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2016-08-16 02:41:15 +0200

    Removed typings from the repo

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
-/// <reference path="modules/sinon-chai/index.d.ts" />
-/// <reference path="modules/sinon/index.d.ts" />
-/// <reference path="modules/source-map-support/index.d.ts" />
